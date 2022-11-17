65906e631b499b090c00cbf661154a87efa63b91
Coli-co <awestlifeblue@gmail.com>
2022-11-17 20:20:53 +0800

    add relations between User and Todo models

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const { route } = require('./models/home')
 const router = express.Router()
-const home = require('./models/home')
-const todos = require('./models/todos')
-const users = require('./models/users')
+const home = require('./modules/home')
+const todos = require('./modules/todos')
+const users = require('./modules/users')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/todos', authenticator, todos)
